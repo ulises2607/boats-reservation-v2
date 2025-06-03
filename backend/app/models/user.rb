cc52c15412ebccc2327456061d97ef9263cdb52f
Ulises Cappa <ulisescappa@gmail.com>
2025-06-03 16:44:12 -0300

    Add user role.

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   has_many :boats, dependent: :destroy
+  has_many :reservations
 
   include Devise::JWT::RevocationStrategies::JTIMatcher
 
   devise :database_authenticatable, :registerable, :recoverable, :validatable, :jwt_authenticatable,
          jwt_revocation_strategy: self
+
+         enum role: {renter: 0, owner: 1, admin: 2}
 
   validates :name, presence: true, uniqueness: true
 
