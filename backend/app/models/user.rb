b4e58e1b0df6103dc42afa225b4dd43052cde66d
Ulises Cappa <ulisescappa@gmail.com>
2025-07-06 01:02:19 -0300

    Reconfigure local database.

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,29 +1,32 @@
 class User < ApplicationRecord
   has_many :boats, dependent: :destroy
   has_many :reservations
+  enum :role, { rented: 0, owner: 1, admin: 2 }
 
   include Devise::JWT::RevocationStrategies::JTIMatcher
 
   devise :database_authenticatable, :registerable, :recoverable, :validatable, :jwt_authenticatable,
          jwt_revocation_strategy: self
 
-         enum role: {renter: 0, owner: 1, admin: 2}
+  
+         
 
   validates :name, presence: true, uniqueness: true
 
-  def email_required?
-    false
-  end
 
-  def email_changed?
-    false
-  end
+  # def email_required?
+  #   false
+  # end
 
-  def will_save_change_to_email?
-    false
-  end
+  # def email_changed?
+  #   false
+  # end
 
-  def password_required?
-    false
-  end
+  # def will_save_change_to_email?
+  #   false
+  # end
+
+  # def password_required?
+  #   false
+  # end
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_28_214856) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_03_193833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -47,6 +47,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_28_214856) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
